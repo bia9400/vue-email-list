a52ba135b7f995c437571731c3f75209f646a602
bia9400 <ferro.biagio@gmail.com>
2022-06-17 15:40:24 +0200

    End of exercise

--- main.js
+++ main.js
@@ -1,3 +1,32 @@
 new Vue({
-    el:"#app"
+    el: "#app",
+    data: {
+        xa: [],
+        counter:0,
+    },
+    methods: {
+        generateEmail() {
+            
+            axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+                .then((emailGenerata) => {
+                    
+                    this.xa.push(emailGenerata.data.response)
+                    this.counter++
+                    
+                })
+        },
+        generateArray(){
+            for(let i=0;i<10;i++){
+
+                this.generateEmail()
+              
+            }
+        },
+        isgenerated(){
+            if(this.counter==10){return true}
+            else{
+                return false
+            }
+        }
+    },
 })
